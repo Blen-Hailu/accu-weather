93bb2bcb844c74a0b4d5e657e93efc4eff0321fc
Blen-Hailu <belaki2@gmail.com>
2022-11-11 10:15:51 +0200

    add input form set up react app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+//import { useState, useEffect } from 'react';
+import './App.css';
+import InputForm from './components/inputform';
+
+function App (){
+  <InputForm/>
+}
+/*const Weather =() =>{
+  const [search, setSearch]= usestate("");
+  const[data, setData] = useState ([]);
+  const[input,setInput] =useState("")
+  let result = true;
+  useEffect (()=>{
+    const fetchWeather = async () => {
+      const response =await fetch ();
+      if (result){
+        setData(await response.json());
+      }
+      return () => {
+        result =false;
+      }
+  }
+  )
+}fetchWeather();
+},[);
+  return 
+  <div>
+      
+  </div>
+
+/*function App() {
+
+const apiKey = "HCD7jnVkMj96094RX0lsttIYZvoQO848"
+  const {weatherData, setWeatherData} = useState{}
+  const {city, setCity} = useState{""}
+   const getWeather = (event)=>{
+    if(event.key =="Enter"){
+      fetch().then ( 
+        response =>response json{}  ).then (
+          data=> {
+            setWeatherData{data}
+          }
+        )    )
+    }
+   }
+  return (
+    <div className="cityContainer">
+      <input>
+      placeholder= "Enter city"
+      onChange= {e=>setCity {e.target.value)}
+      value ={city}
+      onKeyPress ={getweather}
+      </input>
+    </div>
+  
+  );
+}
+*/
+export default App;
---
+++ src/components/inputForm.jsx
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+
+function InputForm(){ 
+
+  const [city, setCity] =useState('')
+
+  const handleCityChange = event => {
+    setFullName(event.target.value)
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('Thank you!');
+
+  };
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>City</label>
+            <input
+              type="text"
+              name="city"
+              placeholder="Enter Your City Here"
+              onChange={handleCityChange}
+              value={city}
+            />
+              
+          </div>
+          <button type="submit" className = "main-button">
+            Submit
+          </button>
+        </form>
+    </div>
+  )
+}
+export default InputForm;
